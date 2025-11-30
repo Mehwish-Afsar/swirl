010ab03f3da34b80018633204a9ea15f7065f1e0
Mehwish-Afsar <136736539+Mehwish-Afsar@users.noreply.github.com>
2025-11-30 05:27:52 +0500

    Refactor onAttach and make_pretty functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,27 +1,47 @@
 .onAttach <- function(...) {
-  if(length(ls(envir=globalenv())) > 0) {
+  vars_in_workspace <- ls(envir = globalenv())
+  
+  if(length(vars_in_workspace) > 0) {
     packageStartupMessage(
-      make_pretty(s()%N%"Hi! I see that you have some variables saved in your",
-                  s()%N%"workspace. To keep things running smoothly, I recommend you clean up",
-                  s()%N%"before starting swirl.", skip_after=TRUE),
-      make_pretty(s()%N%"Type ls() to see a list of the variables in your workspace.",
-                  s()%N%"Then, type rm(list=ls()) to clear your workspace.", skip_after=TRUE),
-      make_pretty(s()%N%"Type swirl() when you are ready to begin.", skip_after=TRUE)
+      make_pretty(
+        "Hi! I see that you have some variables in your workspace.",
+        "To keep things running smoothly, I recommend cleaning it up before starting swirl.",
+        skip_after = TRUE
+      ),
+      make_pretty(
+        "Type ls() to see a list of the variables in your workspace.",
+        "Then, type rm(list=ls()) to clear your workspace.",
+        skip_after = TRUE
+      ),
+      make_pretty(
+        "Type swirl() when you are ready to begin.",
+        skip_after = TRUE
+      )
     )
   } else {
     packageStartupMessage(
-      make_pretty(s()%N%"Hi! Type swirl() when you are ready to begin.",
-                  skip_after=TRUE)
+      make_pretty(
+        "Hi! Type swirl() when you are ready to begin.",
+        skip_after = TRUE
+      )
     )
   }
   invisible()
 }
 
-make_pretty <- function(..., skip_before=TRUE, skip_after=FALSE) {
-  wrapped <- strwrap(str_c(..., sep = " "),
-                     width = getOption("width") - 2)
-  mes <- str_c("| ", wrapped, collapse = "\n")
+make_pretty <- function(..., skip_before = TRUE, skip_after = FALSE) {
+  # Combine all input strings with a space
+  message_text <- paste(..., collapse = " ")
+  
+  # Wrap text to console width
+  wrapped <- strwrap(message_text, width = getOption("width") - 2)
+  
+  # Add "| " prefix to each line
+  mes <- paste0("| ", wrapped, collapse = "\n")
+  
+  # Add optional newlines
   if(skip_before) mes <- paste0("\n", mes)
-  if(skip_after) mes <- paste0(mes, "\n")
+  if(skip_after)  mes <- paste0(mes, "\n")
+  
   mes
 }
